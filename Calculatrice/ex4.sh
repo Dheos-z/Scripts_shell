02af308da0f656a142116271d3665e98e2103433
Zakaria <yakdi.zak@gmail.com>
2017-03-08 11:39:28 +0100

    Ex4 terminé

--- Calculatrice/ex4.sh
+++ Calculatrice/ex4.sh
@@ -5,9 +5,11 @@
 # Ecrire un script capable de reconnaître la syntaxe nombres/opérateurs
 # Doit commencer par un nombre, puis alterner entre nombre & opérateur
 
-for i in $*
-do
-	case $i in
+for((i=1;i<=$#;i++))
+{
+	BONJOUR='bonjour'
+	
+	case ${!i} in
 		"(")
 		echo "parenthèse ouvrante"
 		;;
@@ -18,28 +20,51 @@ do
 		
 		"+")
 		echo "somme"
+		SALUT[$i]="operation"
 		;;
 		
 		"-")
 		echo "soustraction"
+		SALUT[$i]="operation"
 		;;
 		
 		"x")
 		echo "multiplication"
+		SALUT[$i]="operation"
 		;;
 		
 		"/")
 		echo "division"
+		SALUT[$i]="operation"
 		;;
 		
 		*)
 		# Alors c'est un nombre
 		echo "nombre"
+		SALUT[$i]="nombre"
 		;;
 	esac
+}
 
-# A partir d'ici 
+for((i=2;i<=$#;i++))
+{
+	if [ ${SALUT[$i-1]} = "nombre" ]
+	then
+		if [ ${SALUT[$i]} = "nombre" ]
+		then
+			echo 'Erreur : deux nombres à la suite'
+			exit
+		fi
+	elif [ ${SALUT[$i-1]} = "operation" ]
+	then
+		if [ ${SALUT[$i]} = "operation" ]
+		then
+			echo 'Erreur : deux opérations à la suite'
+			exit
+		fi
+	fi
+}
+
+
 	
-
-
-done
+	
